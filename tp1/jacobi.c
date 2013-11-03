bd07bf27c742424dade4598388ffbd1e5caa9e1b
theCmaker <pierre-loup.pissavy94@orange.fr>
2013-11-03 16:19:48 +0100

    Erreur corrigée

--- tp1/jacobi.c
+++ tp1/jacobi.c
@@ -11,13 +11,9 @@ void jacobi (double** a, double** b, double** xInit, int n, double prec)
   double** ax;
   double** axb;
   cpt=0;
-  for (i=0; i<n; i++)
+  for (i=0; i<n; i++) //initialisation xNext
   {
     xNext[i]= (double*) malloc(sizeof(double));
-    for (j=0; j<1; j++)
-    {
-      xNext[i][j]=xInit[i][j];
-    }
   }
   while (residu>=prec)
   {
@@ -26,13 +22,17 @@ void jacobi (double** a, double** b, double** xInit, int n, double prec)
       double somme1=0, somme2=0;
       for (j=0; j<i; j++) //avant l'élément diagonal
       {
-        somme1=somme1+a[i][j]*xNext[j][0];
+        somme1=somme1+a[i][j]*xInit[j][0];
       }
-      for (j=i+1; j<n; j++)
+      for (j=i+1; j<n; j++) //après l'élément diagonal
       {
-        somme2=somme2+a[i][j]*xNext[j][0]; //après l'élément diagonal
+        somme2=somme2+a[i][j]*xInit[j][0]; 
       }
       xNext[i][0]=(1/a[i][i])*(b[i][0]-somme1-somme2);
+    }
+    for(i=0; i<n; i++)
+    {
+      xInit[i][0] = xNext[i][0] ; //copie de xNext dans xInit pour la prochaine itération
     }
     ax=produitMatriciel(a, xNext, n, n, 1); //ax
     axb=difference(ax, b, n, 1); //ax - b
