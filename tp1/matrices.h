b07c6e64884fbd1ed540cdddf127d546f43b3216
theCmaker <pierre-loup.pissavy94@orange.fr>
2013-11-28 16:53:36 +0100

    Rétablissement de la version de matrices.c pour le 1er rapport

--- tp1/matrices.h
+++ tp1/matrices.h
@@ -9,9 +9,7 @@ double ** solveTriangulaireInf (double** mat, double** vec, int n);
 double ** transpose(double** mat, int n);
 double ** produitMatriciel(double** a, double** b, int n, int m, int o); // Produit AB
 double ** difference (double** a, double** b, int n, int m);
-double ** puissanceMatrice(double** mat, int n, int e); // calcule la matrice à la puissance e avec e différent de 0  et 1 (inutile pour nous)
 double norme (double** x, int n);
 double det (double** mat, int n);
-double trace(double** mat, int n);
 
 #endif
