893c28ca56b91008999f0f9e715ebbc4817b0708
AxelDelsol63 <axel.delsol@laposte.net>
2013-12-16 20:23:22 +0100

    Ajout du code de la vecteur propre que j'avais oublié...

--- tp3/puissancesIt.c
+++ tp3/puissancesIt.c
@@ -219,9 +219,14 @@ void fonctionRapport(double**A,int n, double precision)
   
   printf("Approximation du vecteur propre :\n");
   fprintf(fichier,"Vecteur propre : \n");
+  fprintf(fichier,"\\begin{equation*}\n\\begin{pmatrix}\n");
   
   //remplissage des cases
-  convertMattoLatex(x1,n,1);
+  for(i=0;i<n;i++)
+  {
+    fprintf(fichier,"%f \\\\ \n",x1[i][0]);
+  }
+  fprintf(fichier,"\\end{pmatrix}\n\\label{}\n\\end{equation*}\n");
   
   for (i=0; i<n; i++)
   {
